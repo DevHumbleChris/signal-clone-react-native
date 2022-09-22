4a8a4cec4c30bc8a8922a586664802b70ea113bb
DevHumbleChris <dev.humblechris254@gmail.com>
2022-09-22 14:57:47 +0300

    Added Firebase Config Files

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtowEb9ZY5EOPoBon0u3SlKbsInqf6h3A",
+  authDomain: "signal-rn-clone-f9063.firebaseapp.com",
+  projectId: "signal-rn-clone-f9063",
+  storageBucket: "signal-rn-clone-f9063.appspot.com",
+  messagingSenderId: "1093901887599",
+  appId: "1:1093901887599:web:231768fa683891fd82059a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
