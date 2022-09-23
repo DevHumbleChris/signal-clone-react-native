17bffda99be320caee9077d439f43156f59d53f3
DevHumbleChris <dev.humblechris254@gmail.com>
2022-09-23 09:50:04 +0300

    Added Bottom Navigation

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,8 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Icon } from "@rneui/themed";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import ConversationList from "../components/ConversationList";
+import tw from 'twrnc'
 
 const HomeScreen = ({ navigation }) => {
   return (
@@ -13,6 +15,14 @@ const HomeScreen = ({ navigation }) => {
       }}
     >
       <ConversationList />
+      <View style={tw`flex-row mt-auto p-2 justify-evenly mt-3`}>
+        <TouchableOpacity>
+          <Icon name="chat-processing-outline" type="material-community" color="white" size={35} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Icon name="pencil-box" type="material-community" color="white" size={35} />
+        </TouchableOpacity>
+      </View>
       <StatusBar style="black" backgroundColor="white" />
     </SafeAreaView>
   );
